0397173e0b780ef1ca8f30ce3196bcc1432a7115
Ankit Singh <2501.ankit@gmail.com>
2021-03-07 12:50:18 +0530

    add 404 page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './styles/app.css'
 
 const Login = lazy(() => import('./pages/login'))
 const SignUp = lazy(() => import('./pages/signup'))
+const NotFound = lazy(() => import('./pages/notFound'))
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGNUP} component={SignUp} />
+          <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
